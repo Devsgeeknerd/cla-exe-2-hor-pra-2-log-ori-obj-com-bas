2be517313b8fe5513305e6feb1910e4a5373d2ef
Paulo Vanderley <vanderley.1109@gmail.com>
2024-05-17 17:33:41 -0300

    :scroll: Update file.

---
+++ computador.java
@@ -0,0 +1,51 @@
+package br.com.devsgeeknerd.computadores.classes;
+
+import java.util.Date;
+
+public abstract class computador {
+  private String nome;
+  private double preco;
+  private Date dataDeFabricacao;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public double getPreco() {
+    return preco;
+  }
+
+  public void setPreco(double preco) {
+    this.preco = preco;
+  }
+
+  public Date getDataDeFabricacao() {
+    return dataDeFabricacao;
+  }
+
+  public void setDataDeFabicacao(Date dataDeFabricacao) {
+    this.dataDeFabricacao = dataDeFabricacao;
+  }
+
+  public computador(String nome, double preco, Date dataDeFabricacao) {
+    this.nome = nome;
+    this.preco = preco;
+    this.dataDeFabricacao = dataDeFabricacao;
+  }
+
+  public void ligar() {
+    System.out.println(nome + " esta ligando...");
+  }
+
+  public void desligar() {
+    System.out.println(nome + " esta desligando...");
+  }
+
+  public void acessarInternet() {
+    System.out.println(nome + "esta acessando a internet...");
+  }
+}
