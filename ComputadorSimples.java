1abbec811fef848fb2b89b926259a6f8c56d021f
Paulo Vanderley <vanderley.1109@gmail.com>
2024-05-17 18:32:59 -0300

    :scroll: Update file.

--- ComputadorSimples.java
+++ ComputadorSimples.java
@@ -6,4 +6,8 @@ public class ComputadorSimples extends Computador {
   public ComputadorSimples(String nome, double preco, Date dataDeFabricacao) {
     super(nome, preco, dataDeFabricacao);
   }
+
+  public void jogarPaciencia() {
+    System.out.println(getNome() + " esta jogando Paciência...");
+  }
 }
---
+++ Programa.java
@@ -0,0 +1,17 @@
+package br.com.devsgeeknerd.computadores.app;
+
+import java.util.Date;
+
+import br.com.devsgeeknerd.computadores.classes.Computador;
+import br.com.devsgeeknerd.computadores.classes.ComputadorSimples;
+import br.com.devsgeeknerd.computadores.classes.ComputadorParrudo;
+import br.com.devsgeeknerd.computadores.classes.ComputadorGamer;
+
+public class Programa {
+  public static void main(String[] args) {
+    ComputadorSimples computadorSimples = new ComputadorSimples("Computador Home", 1500.00, new Date());
+    ComputadorParrudo computadorParrudo = new ComputadorParrudo("Computador Profissional", 2800.00, new Date(),
+        "Branco");
+    ComputadorGamer computadorGamer = new ComputadorGamer("Computador Gamer", 4200.00, new Date(), 12);
+  }
+}
